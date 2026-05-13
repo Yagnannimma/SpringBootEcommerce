eadccdb7259a5dacfda45359216596455e5acacd
YagnanNimma <yagnannimma@gmail.com>
2026-05-13 09:58:26 +0530

    adding logic to handle addresses

---
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -0,0 +1,37 @@
+package com.ecommerce.project.controller;
+
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import com.ecommerce.project.service.AddressService;
+import com.ecommerce.project.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AddressController {
+
+
+    @Autowired
+    AuthUtil authUtil;
+
+
+    @Autowired
+    AddressService addressService;
+
+    public ResponseEntity<AddressDTO> createAddress(AddressDTO addressDTO){
+        User user = authUtil.loggedInUser();
+        AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
+        return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/address")
+    public ResponseEntity<AddressDTO> getAllAddresses(){
+
+    }
+}
---
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.service;
+
+
+import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService{
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    public AddressDTO createAddress(AddressDTO addressDTO, User user) {
+        Address address = modelMapper.map(addressDTO, Address.class);
+        List<Address> addressList = user.getAddresses();
+        addressList.add(address);
+        user.setAddresses(addressList);
+        address.setUsers(user);
+        return null;
+    }
+}
