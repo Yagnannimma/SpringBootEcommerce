b77119f76aade205ad19b3e961bac2419f063c11
YagnanNimma <yagnannimma@gmail.com>
2026-05-14 08:05:48 +0530

    adding logic to handle addresses

--- src/main/java/com/ecommerce/project/controller/AddressController.java
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -5,12 +5,13 @@ import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
 import com.ecommerce.project.service.AddressService;
 import com.ecommerce.project.util.AuthUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -24,14 +25,30 @@ public class AddressController {
     @Autowired
     AddressService addressService;
 
-    public ResponseEntity<AddressDTO> createAddress(AddressDTO addressDTO){
+    @PostMapping("/addresses")
+    public ResponseEntity<AddressDTO> createAddress(@Valid @RequestBody AddressDTO addressDTO){
         User user = authUtil.loggedInUser();
         AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
         return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("/address")
-    public ResponseEntity<AddressDTO> getAllAddresses(){
-
+    public ResponseEntity<List<AddressDTO>> getAllAddresses(){
+        List<AddressDTO> addressList = addressService.getAddresses();
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
     }
+
+    @GetMapping("/addresses/{addressId}")
+    public ResponseEntity<AddressDTO> getAddressesById(@PathVariable Long addressId){
+        AddressDTO addressDTO = addressService.getAddressesById(addressId);
+        return new ResponseEntity<>(addressDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/addresses")
+    public ResponseEntity<List<AddressDTO>> getUserAddress(){
+        User user = authUtil.loggedInUser();
+        List<AddressDTO> addressDTO = addressService.getUserAddresses(user);
+        return new ResponseEntity<>(addressDTO, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -1,14 +1,19 @@
 package com.ecommerce.project.service;
 
 
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Address;
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
+import com.ecommerce.project.repositories.AddressRepository;
+import com.ecommerce.project.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService{
@@ -16,13 +21,50 @@ public class AddressServiceImpl implements AddressService{
     @Autowired
     ModelMapper modelMapper;
 
+    @Autowired
+    AddressRepository addressRepository;
+
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public AddressDTO createAddress(AddressDTO addressDTO, User user) {
         Address address = modelMapper.map(addressDTO, Address.class);
         List<Address> addressList = user.getAddresses();
         addressList.add(address);
         user.setAddresses(addressList);
-        address.setUsers(user);
-        return null;
+        address.setUser(user);
+        Address savedAddress = addressRepository.save(address);
+        return modelMapper.map(savedAddress, AddressDTO.class);
     }
+
+    @Override
+    public List<AddressDTO> getAddresses() {
+        List<Address> addresses = addressRepository.findAll();
+
+        return addresses.stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AddressDTO getAddressesById(Long addressId) {
+
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address","addressId",addressId));
+
+        return modelMapper.map(address,AddressDTO.class);
+    }
+
+    @Override
+    public List<AddressDTO> getUserAddresses(User user) {
+        List<Address> addresses = user.getAddresses();
+
+        return addresses.stream()
+                .map(address -> modelMapper.map(address,AddressDTO.class))
+                .toList();
+    }
+
+
 }
