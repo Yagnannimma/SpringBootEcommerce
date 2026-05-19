96ebbcac7d79214e695158887b622e007c7fd6b9
YagnanNimma <yagnannimma@gmail.com>
2026-05-19 09:09:35 +0530

    updating logic for Orders

---
+++ src/main/java/com/ecommerce/project/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.payload.OrderDTO;
+
+public interface OrderService {
+
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
